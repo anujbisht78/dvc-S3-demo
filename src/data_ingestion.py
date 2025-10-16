58838c3e8b20f7833cecfafcc0527334609bc960
anujbisht78 <anujbisht3540@gmail.com>
2025-10-16 11:45:33 +0530

    initial commit with PCA 2

---
+++ src/data_ingestion.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import os
+
+df=pd.read_csv('data/raw/Placement.csv')
+
+# seperating features and target variable
+x=df.drop(columns=['Placed'])
+y=df['Placed']
+
+# scaling the features
+scaler=StandardScaler()
+x_scaled=scaler.fit_transform(x)
+
+# applying PCA
+pca=PCA(n_components=2)
+x_pca=pca.fit_transform(x_scaled)
+
+# creating a new dataframe with PCA components
+pca_df=pd.DataFrame(data=x_pca, columns=['PCA1', 'PCA2'])
+pca_df['Placed']=y.values
+
+# saving the processed data
+pca_df.to_csv(os.path.join('data','processed','student_performance_pca.csv'), index=False)
